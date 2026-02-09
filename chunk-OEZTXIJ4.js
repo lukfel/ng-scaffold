099a795b5739bf8a62ff188d9ac231465ca19310
Lukas <dev.lukasfelbinger@gmail.com>
2026-02-09 23:28:45 +0100

    Auto-generated commit

---
+++ chunk-OEZTXIJ4.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunk-VRTXV7B5.js";import"./chunk-MKBGVXGX.js";import"./chunk-3KPH4Y6L.js";var n=[{path:"",component:o}];export{n as NOT_FOUND_ROUTES};
--- chunk-UZIM6ORP.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./chunk-Z2EUHSBE.js";import"./chunk-QG4EUD4Z.js";import"./chunk-3KPH4Y6L.js";var n=[{path:"",component:o}];export{n as NOT_FOUND_ROUTES};
