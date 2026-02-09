b015fe284f0fe5bcf5dfa219a9688d5033b3642d
Lukas <dev.lukasfelbinger@gmail.com>
2026-02-09 23:28:50 +0100

    create release 21.1.28

--- projects/ng-scaffold/src/lib/shared/components/dialogs/confirm-dialog/confirm-dialog.component.ts
+++ projects/ng-scaffold/src/lib/shared/components/dialogs/confirm-dialog/confirm-dialog.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { ConfirmDialogConfig } from '../../../../models';
@@ -7,7 +7,7 @@ import { ConfirmDialogConfig } from '../../../../models';
   selector: 'lf-confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
   styleUrls: ['./confirm-dialog.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [
     MatDialogModule,
