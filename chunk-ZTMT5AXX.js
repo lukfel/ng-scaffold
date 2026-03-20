7497a81e4e1fe28f9f68173a56f2baa2dcbfe11d
Lukas <dev.lukasfelbinger@gmail.com>
2026-03-20 13:26:27 +0100

    Auto-generated commit

--- chunk-ZTMT5AXX.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./chunk-LDPXOTOX.js";import"./chunk-RACX26A6.js";import"./chunk-MSJRYGNQ.js";import"./chunk-HA3ZILUO.js";var n=[{path:"",component:o}];export{n as NOT_FOUND_ROUTES};
