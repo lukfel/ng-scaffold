9389a93017e6b251233fafe99ce315cc1c6d4a0b
Lukas <dev.lukasfelbinger@gmail.com>
2026-03-21 01:34:55 +0100

    Auto-generated commit

--- chunk-PNDW54LO.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./chunk-FNGJBM27.js";import"./chunk-XXNBKME4.js";import"./chunk-3CWXOOLJ.js";import"./chunk-KG4HPYRX.js";var n=[{path:"",component:o}];export{n as NOT_FOUND_ROUTES};
